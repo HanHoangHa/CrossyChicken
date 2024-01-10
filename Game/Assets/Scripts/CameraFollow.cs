56a133df2da8d5d7cff06f6ac5a6525e9b047dfb
DuyPham59 <pduy99295@gmail.com>
2024-01-10 22:59:14 +0700

    update

--- Game/Assets/Scripts/CameraFollow.cs
+++ Game/Assets/Scripts/CameraFollow.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
@@ -32,38 +31,3 @@ public class CameraFollow : MonoBehaviour
     }
 
 }
-=======
-using UnityEngine;
-
-public class CameraFollow : MonoBehaviour
-{
-    // Start is called before the first frame update
-    [SerializeField]
-    private GameObject player;
-    Vector3 cameraPos;
-    Vector3 playerPos;
-    private AudioSource cameraAudio;
-    private Move moveScript;
-    void Start()
-    {
-        cameraPos = transform.position;
-        playerPos = player.transform.position;
-        cameraAudio = GetComponent<AudioSource>();
-        moveScript = GameObject.Find("Chicken").GetComponent<Move>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (moveScript.gameOver)
-        {
-            cameraAudio.Stop();
-        }
-    }
-    private void LateUpdate()
-    {
-        transform.position = new Vector3(cameraPos.x, cameraPos.y + player.transform.position.y - playerPos.y, cameraPos.z);
-    }
-
-}
->>>>>>> dfa6395af90c91800df35947daae937bdf6a45c6
