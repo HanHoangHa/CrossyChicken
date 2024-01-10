dfab6fa9df83ff3cc99c401b2f89801c943fa075
draw1 <90817893+draw1@users.noreply.github.com>
2024-01-11 02:30:22 +0700

    last update
    last update

---
+++ Game/Game/Assets/Scripts/Destroy.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    private float posYChicken;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x > 22 || transform.position.x < -22)
+        {
+            Destroy(gameObject);
+        }
+        posYChicken = GameObject.Find("Chicken").transform.position.y;
+        if (posYChicken - transform.position.y > 10)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
