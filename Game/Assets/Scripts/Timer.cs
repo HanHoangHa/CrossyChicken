8d402dda8deb7828d4150bb25a2307483f5a0e19
DuyPham59 <pduy99295@gmail.com>
2024-01-15 12:57:00 +0700

    update

--- Game/Assets/Scripts/Timer.cs
+++ Game/Assets/Scripts/Timer.cs
@@ -1,64 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
-using UnityEngine.SceneManagement;
+using UnityEngine.PlayerLoop;
 
 public class Timer : MonoBehaviour
 {
-    [SerializeField] TextMeshProUGUI timerText;
-    [SerializeField] float remainingTime;
-    public GameObject gameOverUI;
-
-    // Thêm biến và tham chiếu liên quan đến script Move
-    private Move moveScript;
-    private float posYChicken;
-    private AudioSource audioSource;
+    Move moveScript;
+    AudioSource audioSource;
     public AudioClip death;
-
-    private bool gameOverTriggered = false; // Thêm biến để đảm bảo TriggerGameOver chỉ được gọi một lần
-
+    public GameObject gameOverUI;
+    private float timeUpdate;
+    private float distanceTime;
     void Start()
     {
-        // Thực hiện khởi tạo biến và tham chiếu ở đây
+        timeUpdate = Time.time;
         moveScript = GetComponent<Move>();
         audioSource = GetComponent<AudioSource>();
-        posYChicken = transform.position.y;
     }
 
     void Update()
     {
-        if (remainingTime > 0 && !gameOverTriggered) // Thêm điều kiện kiểm tra gameOverTriggered
+        distanceTime = Time.time - timeUpdate;
+        if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.S)|| Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D) && moveScript.gameOver == false)
         {
-            remainingTime -= Time.deltaTime;
-            UpdateTimerDisplay(remainingTime);
+            timeUpdate = Time.time;
         }
-        else
+        if(distanceTime > 5 && moveScript.gameOver == false)
         {
-            if (remainingTime != 0 && !gameOverTriggered)
-            {
-                remainingTime = 0;
-                UpdateTimerDisplay(remainingTime);
-                TriggerGameOver();
-            }
+            moveScript.gameOver = true;
+            Debug.Log(moveScript.gameOver);
+            audioSource.PlayOneShot(death, 1f);
+            gameOverUI.SetActive(true);
         }
     }
 
-    void UpdateTimerDisplay(float timeToDisplay)
-    {
-        // Loại bỏ phần timeToDisplay += 1;
-        int minutes = Mathf.FloorToInt(timeToDisplay / 60);
-        int seconds = Mathf.FloorToInt(timeToDisplay % 60);
-        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
-    }
-
-    private void TriggerGameOver()
-    {
-        moveScript.gameOver = true;
-        Debug.Log(moveScript.gameOver);
-        posYChicken = transform.position.y; // Gán giá trị mới cho posYChicken
-        audioSource.PlayOneShot(death, 1f);
-        gameOverUI.SetActive(true);
-        gameOverTriggered = true; // Đặt gameOverTriggered thành true để đảm bảo TriggerGameOver chỉ được gọi một lần
-    }
 }
